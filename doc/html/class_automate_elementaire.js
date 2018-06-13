1c6d86faa319dd0793433a389fc2a6b673ee3a4e
Bertille Dubosc <38249914+bertille-ddp@users.noreply.github.com>
2018-06-13 14:47:51 +0200

    Bertille (#2)
    * Mise à jour
    
    * Documentation UI Élémentaire
    
    * Suppression de autocell.pro.user

--- doc/html/class_automate_elementaire.js
+++ doc/html/class_automate_elementaire.js
@@ -1,8 +1,8 @@
 var class_automate_elementaire =
 [
-    [ "AutomateElementaire", "class_automate_elementaire.html#a1c6c24ee242e2ac8749021a3da13667e", null ],
-    [ "AutomateElementaire", "class_automate_elementaire.html#a329ac74131dc3285a6ca843683902bcb", null ],
+    [ "AutomateElementaire", "class_automate_elementaire.html#a90cd1823cc729bf5216e93f0efbf7dab", null ],
     [ "appliquerTransition", "class_automate_elementaire.html#adab333b65d44a50986ddb024bc475121", null ],
     [ "getNumero", "class_automate_elementaire.html#a05462d361cac350a4cc9fdf166bb6534", null ],
-    [ "getNumeroBit", "class_automate_elementaire.html#a239cdaf094b7fd23ec915e90a97e5ca0", null ]
+    [ "getNumeroBit", "class_automate_elementaire.html#a239cdaf094b7fd23ec915e90a97e5ca0", null ],
+    [ "operator=", "class_automate_elementaire.html#a0c73a3eac1799182beca019ac05d4a44", null ]
 ];
