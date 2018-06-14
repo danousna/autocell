4acc41093873f989b24f50374b13cd589b50ab7e
bertille-ddp <bduboscd@etu.utc.fr>
2018-06-14 16:17:53 +0200

    Interface automate Wireworld

--- src/ui/wwview.h
+++ src/ui/wwview.h
@@ -1,7 +1,19 @@
 #ifndef WWVIEW_H
 #define WWVIEW_H
 
+#include <thread>
+#include <chrono>
+#include <stdlib.h>
+#include <time.h>
+
 #include <QWidget>
+#include <QLineEdit>
+#include <QIntValidator>
+#include <QTableWidget>
+#include <QString>
+
+#include "../simulateur.h"
+#include "../automate2d.h"
 
 namespace Ui {
 class WWView;
@@ -10,13 +22,35 @@ class WWView;
 class WWView : public QWidget
 {
     Q_OBJECT
+    Ui::WWView*ui;
+    int voisinsMin;
+    int voisinsMax;
+    int dimensions;
+    int tailleCell;
+    int steps;
+    int stepState;
+    int speed;
+    bool paused;
 
+    // L'automate de cette view. C'est un singleton.
+    AutomateWW* automate;
 public:
     explicit WWView(QWidget *parent = 0);
     ~WWView();
-
-private:
-    Ui::WWView *ui;
+private slots:
+    void toggleCell(QTableWidgetItem* item);
+    void next();
+    void play(int startStep = 0);
+    void togglePlayPause();
+    void reset();
+    void refreshTaille();
+    void syncGrilles(Grille* grilleAutomate, QTableWidget* grilleQT, bool set = true);
+    void viderGrille();
+    void drawGrille(QTableWidget* grille, unsigned int tCell, unsigned int n);
+    void toggleUI();
+    void changeSpeed(int s);
+    void changeSteps(int n);
+    void randomGen();
 };
 
 #endif // WWVIEW_H
