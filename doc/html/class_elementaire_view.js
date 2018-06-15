729915acf361109e7228984e2f0b04f34a53fbfb
bertille-ddp <bduboscd@etu.utc.fr>
2018-06-15 23:28:42 +0200

    Documentation pour l'import/export

--- doc/html/class_elementaire_view.js
+++ doc/html/class_elementaire_view.js
@@ -4,5 +4,5 @@ var class_elementaire_view =
     [ "~ElementaireView", "class_elementaire_view.html#af0b7d67380c978106f43f1459788d0cf", null ],
     [ "getTaille", "class_elementaire_view.html#ad1aa85b65dd6d68552fc2b75be8f36bc", null ],
     [ "import", "class_elementaire_view.html#a5599ac0321e12d652d2b10f0fcd0d167", null ],
-    [ "save", "class_elementaire_view.html#ab519a6d5b85ecd7b5131053b006d8579", null ]
+    [ "save", "class_elementaire_view.html#a9fd54666dd4c5297cccad796b5f076ad", null ]
 ];
--- doc/html/class_main_window.js
+++ doc/html/class_main_window.js
@@ -2,6 +2,9 @@ var class_main_window =
 [
     [ "MainWindow", "class_main_window.html#a8b244be8b7b7db1b08de2a2acb9409db", null ],
     [ "~MainWindow", "class_main_window.html#ae98d00a93bc118200eeef9f9bba1dba7", null ],
+    [ "import", "class_main_window.html#a51a9c29c54bfbfd89ea5daf3c4382709", null ],
+    [ "loadAppState", "class_main_window.html#a42202960940c2305c80cf9d2e50e5564", null ],
+    [ "saveAppState", "class_main_window.html#a94b693e3fa35f35ba200d2427b9cd419", null ],
     [ "elementaireAutomate", "class_main_window.html#a4bb6dfea376839246ae1671ae38bbc8a", null ],
     [ "golAutomate", "class_main_window.html#a1fb36563a8411560a4507b800bd5962e", null ],
     [ "wwAutomate", "class_main_window.html#a48a9a529254a014705a9273f48ac5b97", null ]
